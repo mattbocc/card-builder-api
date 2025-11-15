9c81f3a237dfcee7eae64201d709ff70c3a2bb0d
Matthew Boccalon <matthew.boccalon@live.ca>
2025-11-14 23:40:00 -0500

    feat: added christmas special event to the special_event dictionary for prompt.

--- dictionaries/event_prompts.py
+++ dictionaries/event_prompts.py
@@ -6,6 +6,16 @@ special_event = {
         ],
         "prompt": "Additional Halloween Aesthetic: Despite being heavily Ghibli themed, I want this card to have a halloween theme to it. Please use the attached Ghibli inspired ghost and pumpkin pictures in different forms within the image.",
     },
-    "christmas": "",
+    "christmas": {
+        "images": [
+            "input/poke/halloween/santa-claus.png",
+            "input/poke/halloween/elf.png",
+            "input/poke/halloween/santa-hat.png",
+        ],
+        "prompt": """
+				Additional Christmas Aesthetic: Despite being heavily Ghibli themed, I want this card to have a Christmas theme to it. Add snowy nature background with christmas features littered all over.
+				Please use the attached Ghibli santa-claus and elf image a single time each in the generated picture but also add make any people wear the santa-hats.
+			""",
+    },
     "valentines": "",
 }
